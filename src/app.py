cd78083670ba9c7f3c2ff7177bee3268d439bd39
carsonpassero <cpassero19@gmail.com>
2021-11-30 19:12:20 -0500

    started basic UI with services exec compatibility

--- src/app.py
+++ src/app.py
@@ -1,111 +1,47 @@
 import tkinter as tk
-from tkinter import ttk
+import pickle
+from tkinter import Listbox, ttk
 from tkinter import dnd
+from tkinter.constants import NW, OUTSIDE
+from utils import *
 
 
+available_services = []
+available_things = []
+
+recLine = []
+
+class DragDropListbox(Listbox):
+    """ A Tkinter listbox with drag'n'drop reordering of entries. """
+    def __init__(self, master, **kw):
+        kw['selectmode'] = tk.SINGLE
+        Listbox.__init__(self, master, kw)
+        self.bind('<Button-1>', self.setCurrent)
+        self.bind('<B1-Motion>', self.shiftSelection)
+        self.curIndex = None
+
+    def setCurrent(self, event):
+        self.curIndex = self.nearest(event.y)
+
+    def shiftSelection(self, event):
+        i = self.nearest(event.y)
+        if i < self.curIndex:
+            x = self.get(i)
+            self.delete(i)
+            self.insert(i+1, x)
+            self.curIndex = i
+        elif i > self.curIndex:
+            x = self.get(i)
+            self.delete(i)
+            self.insert(i-1, x)
+            self.curIndex = i
+  
 class ServiceButton():
-    def __init__(self, container, where, name):
-        ttk.Button(container, text='Add Service', 
-                   command=lambda: Draggable(name).attach(where.canvas)).pack()
-
-
-class Reciever:
-    def __init__(self, root, id):
-        self.top = root
-        self.canvas = tk.Canvas(self.top, width=200, height=100)
-        self.canvas.pack()
-        self.canvas.dnd_accept = self.dnd_accept
-        self.id = id
-        self.contains = 0
-
-    def dnd_accept(self, source, event):
-        return self
-
-    def dnd_enter(self, source, event):
-        self.canvas.focus_set() # Show highlight border
-        x, y = source.where(self.canvas, event)
-        x1, y1, x2, y2 = source.canvas.bbox(source.id)
-        dx, dy = x2-x1, y2-y1
-        self.dndid = self.canvas.create_rectangle(x, y, x+dx, y+dy)
-        self.dnd_motion(source, event)
-
-    def dnd_motion(self, source, event):
-        x, y = source.where(self.canvas, event)
-        x1, y1, x2, y2 = self.canvas.bbox(self.dndid)
-        self.canvas.move(self.dndid, x-x1, y-y1)
-
-    def dnd_leave(self, source, event):
-        self.top.focus_set() # Hide highlight border
-        self.canvas.delete(self.dndid)
-        self.dndid = None
-
-    def dnd_commit(self, source, event):
-        self.dnd_leave(source, event)
-        x, y = source.where(self.canvas, event)
-        source.attach(self.canvas, x, y)
-        self.contains += 1
-        print("Block is in Receiver #" + str(self.id) + ' which now contains ' + str(self.contains) + ' block(s).')
-
-
-class Draggable:
-    def __init__(self, name):
-        self.name = name
-        self.canvas = self.label = self.id = None
-
-    def attach(self, canvas, x=10, y=10):
-        if canvas is self.canvas:
-            self.canvas.coords(self.id, x, y)
-            return
-        if self.canvas is not None:
-            self.detach()
-        if canvas is None:
-            return
-        label = tk.Label(canvas, text=self.name,
-                              borderwidth=2, relief="groove")
-        id = canvas.create_window(x, y, window=label, anchor="nw")
-        self.canvas = canvas
-        self.label = label
-        self.id = id
-        label.bind("<ButtonPress>", self.press)
-
-    def detach(self):
-        canvas = self.canvas
-        if canvas is None:
-            return
-        id = self.id
-        label = self.label
-        self.canvas = self.label = self.id = None
-        canvas.delete(id)
-        label.destroy()
-
-    def press(self, event):
-        if dnd.dnd_start(self, event):
-            # where the pointer is relative to the label widget:
-            self.x_off = event.x
-            self.y_off = event.y
-            # where the widget is relative to the canvas:
-            self.x_orig, self.y_orig = self.canvas.coords(self.id)
-
-    def move(self, event):
-        x, y = self.where(self.canvas, event)
-        self.canvas.coords(self.id, x, y)
-
-    def putback(self):
-        self.canvas.coords(self.id, self.x_orig, self.y_orig)
-
-    def where(self, canvas, event):
-        # where the corner of the canvas is relative to the screen:
-        x_org = canvas.winfo_rootx()
-        y_org = canvas.winfo_rooty()
-        # where the pointer is relative to the canvas widget:
-        x = event.x_root - x_org
-        y = event.y_root - y_org
-        # compensate for initial pointer offset
-        return x - self.x_off, y - self.y_off
-
-    def dnd_end(self, target, event):
-        pass
-
+    def __init__(self, container, where: DragDropListbox, name):
+        self.button = ttk.Button(container, text='Add Service', 
+                   command=lambda:where.insert(0, name)).pack()
+    #Change TODO
+    
 
 class App(tk.Tk):
     def __init__(self):
@@ -114,6 +50,9 @@ class App(tk.Tk):
         # configure the root window
         self.title('Atlas IoT IDE')
         self.geometry('1200x600')
+        
+        
+
 
         # splits the window into two columns with the second one
         # being twice the size of the first given by the weight
@@ -142,33 +81,86 @@ class App(tk.Tk):
         info.add(apps, text='Apps')
         
         # Recipe editor / just a blank frame aligned left
-        recipies = tk.Frame(self, bg='pink')
-        recipies.grid(row=0, column=1, sticky=tk.NSEW)
+        recipes = tk.Frame(self, bg='dark grey')
+        recipes.grid(row=0, column=1, sticky=tk.NSEW)
+
+        d = DragDropListbox(recipes)
+        d.pack()
+        d.place(bordermode=OUTSIDE, anchor=NW, height=500, width=250, rely=0.05, relx=0.0025)
+
+        #Textbox for service arguments
+        inputtxt = tk.Text(recipes, height=5, width=20).pack(side=tk.LEFT)
+        txtRules = tk.Label(recipes, text="Comma for args, SemiColon for func", fg="grey").pack(side=tk.LEFT)
 
         # Adding some buttons to the recipe view and a title
-        ttk.Label(recipies, text ="Recipes: This is where the services are dragged and arranged").pack()
-        ttk.Button(recipies, text='Save').pack(side=tk.RIGHT)
-        ttk.Button(recipies, text='Clear').pack(side=tk.RIGHT)
+        ttk.Label(recipes, text ="Recipes: This is where the services are dragged and arranged").pack()
+        ttk.Button(recipes, text='Save').pack(side=tk.RIGHT)
 
-        # Makes 4 places to add 'code blocks' to
-        areas = [Reciever(recipies, i) for i in range(4)]
+        output = tk.Text(recipes, state='disabled', width=44, height=20)
+        output.pack()
+        
+        def clear():
+            d.delete(0,tk.END)
+            recLine.clear()
+            output.configure(state='normal')
+            output.delete('1.0', 'end')
+            output.configure(state='disabled')
+
+        clr = ttk.Button(recipes, text='Clear', command=clear)
+        clr.pack()
+        
+        
+        
+        def run():
+            for entry in d.get(0, tk.END):
+                recLine.append(entry)
+
+            for s in recLine:
+                for sobj in available_services:
+                    if s == sobj.name:
+                        result = sobj.exec([])
+                        if sobj.has_output:
+                            output.configure(state='normal')
+                            output.insert('end', result)
+                            output.configure(state='disabled')
+                        break
+                
+        ttk.Button(recipes, text='Run', command=run).pack(side=tk.RIGHT)
+        
+        # def run():
+        #     #implement run function
+        # ttk.Button(recipies, text='Run', command=clear).pack(side=tk.RIGHT)
+
+        
+
 
         # Buttons and descriptions for services that can be pressed to add them to the 
         # recipe area.
-        thing_name = 'RaspberryPi 4'
-        space_name = 'My Virtual Smart Space 1'
+        
+        print(len(available_things), len(available_services))
+        for s in available_services:
+            print(s.name)
+            ttk.Label(services, text='Name: ' + s.name).pack()
+            ttk.Label(services, text='Thing: ' + s.thing.id).pack()
+            ttk.Label(services, text='Smart Space: ' + s.thing.space).pack()
+            ServiceButton(services, d, s.name)
+            
+       
+        
 
-        ttk.Label(services, text='Turn on Light').pack()
-        ttk.Label(services, text=thing_name).pack()
-        ttk.Label(services, text=space_name).pack()
-        ServiceButton(services, areas[0], 'Turn on Light')
-
-        ttk.Label(services, text='Turn off Light').pack()
-        ttk.Label(services, text=thing_name).pack()
-        ttk.Label(services, text=space_name).pack()
-        ServiceButton(services, areas[0], 'Turn off Light')
 
 
 if __name__ == "__main__":
+    # tweets = [json_to_tweet(t) for t in listen_for_json()]
+    # available_things = tweets_to_things(tweets)
+    # available_services = tweets_to_services(tweets)
+
+    with open('services.txt', 'rb') as f:
+        available_services = pickle.load(f)
+
+    with open('things.txt', 'rb') as f:
+        available_things = pickle.load(f)
+    
     app = App()
     app.mainloop()
+    
